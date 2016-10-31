36f82d6a8ad906f81336381a5929a8345f5865a4
yenly <yencodes@gmail.com>
2016-10-31 11:10:38 -0700

    Add video list component to query Youtube Api for surfboards video and display length to test connection.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import key from './components/api_key';
+const API_KEY = key;
 
-// Create new component. This component should produce some HTML
-const App = () => { // ES6 way of defining a function; same as function ()
-  return <div>Hi</div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it

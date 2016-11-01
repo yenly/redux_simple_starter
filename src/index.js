58541a3c24770ba60ef4f36641800644018b58dd
yenly <yencodes@gmail.com>
2016-10-31 17:22:28 -0700

    Add video detail component to embed video

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import key from './components/api_key';
 const API_KEY = key;
 
@@ -21,6 +22,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[3]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
